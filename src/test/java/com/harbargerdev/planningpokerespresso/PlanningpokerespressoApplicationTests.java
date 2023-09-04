55f193cadebd7edb81d1695a95e67551ded78be7
harbargerdev <aharbarger@gmail.com>
2023-09-04 14:53:59 -0400

    Spring Boot initializer files

---
+++ src/test/java/com/harbargerdev/planningpokerespresso/PlanningpokerespressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harbargerdev.planningpokerespresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningpokerespressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
